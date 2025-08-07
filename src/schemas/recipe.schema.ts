a02e2d3d054826ed7356e36cb0c0574dfd96a78e
Dmytro Perepelytsa <umniyd@gmail.com>
2025-08-07 17:13:31 +0300

    feat: small fix

--- src/schemas/recipe.schema.ts
+++ src/schemas/recipe.schema.ts
@@ -6,7 +6,7 @@ export const recipeSchema = z.object({
   description: z.string().max(360, 'Description must be at most 360 characters'),
   instructions: z.string().min(2, 'Instructions must be at least 2 characters').max(360, 'Instructions must be at most 360 characters'),
   prepTime: z
-    .number()
+    .number('Should be a number')
     .min(1, 'Must be at least 1 minute')
     .max(9999, 'Must be at most 9999 minutes'),
   difficulty: z.enum(RecipeDifficulty, 'Difficulty must be one of Easy, Medium, or Hard'),
