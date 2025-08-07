3cc2ac7fc236ecf5b6aab140439501a41083ba08
Dmytro Perepelytsa <umniyd@gmail.com>
2025-08-07 16:30:42 +0300

    feat: added possibility to search by name

--- src/pages/RecipesPage.tsx
+++ src/pages/RecipesPage.tsx
@@ -10,7 +10,8 @@ interface RecipesPageProps {
 function RecipesPage({ userRecipes }: RecipesPageProps) {
   const [errorMessage, setErrorMessage] = useState<null | string>(null);
   const [recipes, setRecipes] = useState<Recipe[]>([]);
-
+  const [searchFilter, setSearchFilter] = useState<string>('');
+  
   useEffect(() => {
     const getRecipes = async () => {
       try {
@@ -24,10 +25,20 @@ function RecipesPage({ userRecipes }: RecipesPageProps) {
     getRecipes();
   }, [userRecipes]);
 
+  const filteredRecipes = recipes.filter(recipe =>
+    recipe.title.toLowerCase().includes(searchFilter.toLowerCase())
+  );
+
   return (
     <div className='pt-20 px-10'>
+      <input
+        onChange={(e) => setSearchFilter(e.target.value)}
+        type="text"
+        placeholder="Search recipes..."
+        className="w-full border border-slate-300 bg-white outline-slate-400 px-3 py-2 rounded mb-10 max-w-[600px] mx-auto block"
+      />
       <ul className='flex flex-col gap-5 items-center'>
-        {recipes.map((recipe, index) => <RecipeCard key={recipe.id} recipe={recipe} index={index}></RecipeCard>)}
+        {filteredRecipes.map((recipe, index) => <RecipeCard key={recipe.id} recipe={recipe} index={index}></RecipeCard>)}
       </ul>
       {errorMessage && (
         <p className="text-red-500 text-2xl mt-2 text-center font-medium">{errorMessage}</p>
